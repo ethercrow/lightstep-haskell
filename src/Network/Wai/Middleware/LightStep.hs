71d7fc84c5d3408942b5bc6875d705d668b8abd9
Dmitry Ivanov <ethercrow@gmail.com>
2019-11-03 22:57:10 +0100

    Update span tags in WAI middleware

--- src/Network/Wai/Middleware/LightStep.hs
+++ src/Network/Wai/Middleware/LightStep.hs
@@ -8,13 +8,17 @@ import qualified Data.Text.Encoding as T
 import Network.Wai
 import Network.HTTP.Types
 
+-- Semantic conventions for HTTP spans:
+-- https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/data-http.md
+
 tracingMiddleware :: Application -> Application
-tracingMiddleware app = \req sendResp ->
+tracingMiddleware app = \req sendResp -> do
   withSpan "WAI handler" $ do
     setTag "span.kind" "server"
+    setTag "component" "http"
     setTag "http.method" $ T.decodeUtf8 (requestMethod req)
-    setTag "http.path" $ T.decodeUtf8 (rawPathInfo req)
-    setTag "http.version" $ showT (httpVersion req)
+    setTag "http.target" $ T.decodeUtf8 (rawPathInfo req)
+    setTag "http.flavor" $ showT (httpVersion req)
     app req $ \resp -> do
       setTag "http.status_code" $ showT (statusCode $ responseStatus resp)
       sendResp resp
