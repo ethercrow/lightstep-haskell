2938e0c49e27ad82bb722342a39296a3cd7bb9ce
Dmitry Ivanov <ethercrow@gmail.com>
2019-12-21 22:56:54 +0100

    Change the wai example respond with memory usage

--- examples/wai/Main.hs
+++ examples/wai/Main.hs
@@ -1,22 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import qualified Data.ByteString.Lazy.Char8 as LBS
 import Data.Maybe (fromMaybe)
+import qualified Data.Text as T
+import GHC.Stats
+import LightStep.HighLevel.IO (LightStepConfig (..), withSingletonLightStep)
+import Network.HTTP.Types
+import Network.Wai
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.LightStep
 import System.Environment
 import System.Exit
-import LightStep.HighLevel.IO (LightStepConfig (..), withSingletonLightStep)
-import Network.Wai.Middleware.LightStep
-import Network.Wai
-import Network.HTTP.Types
-import Network.Wai.Handler.Warp (run)
-import qualified Data.Text as T
 
 robustScalableProductionReadyMicroservice :: Application
 robustScalableProductionReadyMicroservice = \_req respond -> do
-  putStrLn "Disrupting the industry"
-  respond $ responseLBS
-    status200
-    [("Content-Type", "text/plain")]
-    "It's all data!"
+  rtsStats <- getRTSStats
+  respond $
+    responseLBS
+      status200
+      [("Content-Type", "text/plain")]
+      (LBS.pack $ "RAM usage: " <> show (max_live_bytes rtsStats `div` 1_000) <> " KB")
 
 main :: IO ()
 main = do
